9109595a17023df79b80ab33b27fc800fcebc5f2
Ajay Nikam <128999329+AjayNikam404@users.noreply.github.com>
2025-11-16 11:40:11 +0530

    New project added calc

---
+++ Calculator-1/src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calculator 
+{
+	@GetMapping("calc/add/{a}/{b}")
+	int add(@PathVariable int a,@PathVariable int b)
+	{
+		return a+b;
+	}
+	
+	@GetMapping("calc/sub/{a}/{b}")
+	int sub(@PathVariable int a,@PathVariable int b)
+	{
+		return a-b;
+	}
+	
+	@GetMapping("calc/mul/{a}/{b}")
+	int mul(@PathVariable int a,@PathVariable int b)
+	{
+		return a*b;
+	}
+	
+	@GetMapping("calc/div/{a}/{b}")
+	int div(@PathVariable int a,@PathVariable int b)
+	{
+		return a+b;
+	}
+	
+
+}
